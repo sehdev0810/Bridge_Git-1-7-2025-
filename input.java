afa391cf64d6e29d07fc3e8b93dff4e8ca01bc44
Sehdev <samarthsehdev502@gmail.com>
2025-07-01 12:20:06 +0530

    first commit

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Name :");
+        String input =sc.nextLine();
+        System.out.println(input);
+    }
+}
